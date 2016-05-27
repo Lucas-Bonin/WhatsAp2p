23629eac1b2be2b6354f067a50c82831798f9e62
Lucas Bonin <lucasbonin369@gmail.com>
2016-05-26 23:41:14 -0300

    Message encoder and decoder

---
+++ ClientWhatsAp2p/ClientWhatsAp2p/Decoders.c
@@ -0,0 +1,9 @@
+//
+//  Decoders.c
+//  ClientWhatsAp2p
+//
+//  Created by Lucas Bonin on 5/26/16.
+//  Copyright © 2016 Lucas Bonin. All rights reserved.
+//
+
+#include "Decoders.h"
---
+++ ClientWhatsAp2p/ClientWhatsAp2p/Decoders.h
@@ -0,0 +1,14 @@
+//
+//  Decoders.h
+//  ClientWhatsAp2p
+//
+//  Created by Lucas Bonin on 5/26/16.
+//  Copyright © 2016 Lucas Bonin. All rights reserved.
+//
+
+#ifndef Decoders_h
+#define Decoders_h
+
+#include <stdio.h>
+
+#endif /* Decoders_h */
